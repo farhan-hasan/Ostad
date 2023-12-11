437f48d2abc5cfb164a3e7c2a30fad4cbbf14acd
Farhan Hasan <syedfarhan6491232@gmail.com>
2023-12-11 22:33:24 +0600

    test push

--- live_class/ostad.dart
+++ live_class/ostad.dart
@@ -1,5 +1,19 @@
 import 'package:ostad/ostad.dart' as ostad;
 
 void main(List<String> arguments) {
-  print('Hello world: ${ostad.calculate()}!');
+  Set<int> st = {1, 2, 2};
+  print(st);
+  Map<int, int> mp = {1: 2, 2: 3, 3: 2};
+
+  mp.forEach((k, v) {
+    print('${k}: ${v}');
+  });
+
+  List<int> list = [];
+  list.add;
+  list.addAll([2, 3, 4]);
+
+  for (int i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
 }
